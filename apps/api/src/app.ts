cc60f49952ac9b3e064246175bb0342aa0b2c720
Hildemar Gonzalez <hildemargonzalez08@gmail.com>
2025-12-17 18:49:24 -0400

    feat(api): add movie resource with filtering
    - Add GET /movies endpoint with query parameter support (optional genre filter)
    - Add GET /movies/:imdb_id endpoint for single resource access (IMDb ID parameter)
    - Disable 'x-powered-by' header to enhance security

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,12 +1,40 @@
 import express, { type Express, type Request, type Response } from "express";
+import movies from "./data/movies.json" with { type: "json" };
 import "dotenv/config";
 
 const app: Express = express();
+
+app.disable("x-powered-by");
 
 const PORT = process.env.PORT ?? 3001;
 
 app.get("/", (_req: Request, res: Response) => {
     res.status(200).send("¡Servidor de películas operativo!");
+});
+
+app.get("/movies", (req: Request, res: Response) => {
+  const { genre } = req.query;
+  
+  if (genre) {
+    if (typeof genre !== "string") return res.status(400).json({ error: "Genre parameter must be a string" });
+    
+    const genreLower = genre.toLowerCase();
+    
+    const filteredMovies = movies.filter(movie => movie.translations.en.genre.some((g: string) => g.toLowerCase() === genreLower));
+    return res.json(filteredMovies);
+  }
+  
+  res.json(movies);
+});
+
+app.get("/movies/:imdb_id", (req: Request, res: Response) => {
+  const { imdb_id } = req.params;
+  
+  const movie = movies.find(movie => movie.imdb_id === imdb_id);
+  
+  if (movie) return res.json(movie);
+  
+  res.status(404).json({ message: "Movie not found!" });
 });
 
 app.listen(PORT, () => {
