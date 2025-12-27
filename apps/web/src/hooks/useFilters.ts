310e2af74e56449276b77adfd173069c06e29696
Hildemar Gonzalez <hildemargonzalez08@gmail.com>
2025-12-27 16:09:45 -0400

    feat(validations): add validation functions and error state for input filters in useFilters hook
    - Add state for format error handling on 'year', 'actor' and 'director' inputs as an object
    - Implement validateName to verify that 'actor' and 'director' input text contains only letters and spaces
    - Implement validateYear to verify that the input year is not less than 1900 or greater than the current year
    - Add 'isApplyButtonDisabled' flag to control the active state of the apply filters button
    - These validations will be integrated into the App in a future commit

--- apps/web/src/hooks/useFilters.ts
+++ apps/web/src/hooks/useFilters.ts
@@ -3,6 +3,68 @@ import type { FilterState } from "../types/response.types";
 
 export function useFilters() {
     const [isFilterOpen, setIsFilterOpen] = useState(false);
+    const [filterFormatErrors, setFilterFormatErrors] = useState({
+        year: "",
+        actor: "",
+        director: ""
+    });
+
+    const validateName = (name: "actor" | "director", value: string) => {
+        let error = "";
+
+        const trimmedValue = value.trim();
+
+        const isValidName = (text: string): boolean => {
+            // Regex que permite letras, acentos, ñ, ü y espacios
+            return /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$/.test(text);
+        };
+
+        if (!isValidName(trimmedValue)) {
+            const fieldNames: Record<string, string> = {
+                actor: "Actor",
+                director: "Director"
+            };
+
+            error = `${
+                fieldNames[name] || "field"
+            } can only contain letters and spaces"`;
+        }
+
+        setFilterFormatErrors(prev => ({ ...prev, [name]: error }));
+    };
+
+    const validateYear = (name: "year", value: string) => {
+        let error = "";
+        const trimmedValue = value.trim();
+        const currentYear = new Date().getFullYear();
+
+        if (trimmedValue === "") {
+            setFilterFormatErrors(prev => ({ ...prev, [name]: error }));
+            return;
+        }
+
+        // Patrón para verificar que solo hayan digitos
+        const YEAR_REGEX = /^\d+$/;
+
+        if (!YEAR_REGEX.test(trimmedValue)) {
+            error = "Please enter a valid year";
+        } else {
+            const numberValue = parseInt(trimmedValue, 10);
+
+            if (numberValue < 1900) {
+                error = "Year cannot be earlier than 1900";
+            } else if (numberValue > currentYear) {
+                error = "Year cannot be in the future";
+            }
+        }
+
+        setFilterFormatErrors(prev => ({ ...prev, [name]: error }));
+    };
+
+    const isApplyButtonDisabled =
+        !!filterFormatErrors.year ||
+        !!filterFormatErrors.actor ||
+        !!filterFormatErrors.director;
 
     const initialFiltersState: FilterState = useMemo(
         () => ({
@@ -28,6 +90,9 @@ export function useFilters() {
         setIsFilterOpen,
         filters,
         setFilters,
-        resetFilters
+        resetFilters,
+        validateName,
+        validateYear,
+        isApplyButtonDisabled
     };
 }
