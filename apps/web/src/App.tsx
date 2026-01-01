b4151eca788260767085929d2a28e748b37a4688
Hildemar Gonzalez <hildemargonzalez08@gmail.com>
2026-01-01 19:47:33 -0400

    feat(router): setup dynamic routing and code splitting for details view
    - Add /movie/:imdb_id route to App.tsx
    - Implement React.lazy and Suspense with Loader fallback
    - Optimize bundle size by separating MovieDetails from main chunk

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,24 @@
 import Home from "./views/Home";
+import { Loader } from "./components/Loader";
 import { FiltersProvider } from "./context/FiltersContext";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const MovieDetails = lazy(() => import("./views/MovieDetails"));
 
 function App() {
     return (
         <FiltersProvider>
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
+                <Suspense fallback={<Loader />}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route
+                            path="/movie/:imdb_id"
+                            element={<MovieDetails />}
+                        />
+                    </Routes>
+                </Suspense>
             </BrowserRouter>
         </FiltersProvider>
     );
